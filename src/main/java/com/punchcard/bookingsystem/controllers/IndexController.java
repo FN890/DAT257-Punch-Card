6a119374a113bc69f720fb66a28842c84f515d75
FN <fn_890@icloud.com>
2021-04-12 15:54:24 +0200

    Initial setup: Every mapping that is not under /api is not returned /index.html

---
+++ src/main/java/com/punchcard/bookingsystem/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.punchcard.bookingsystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}"})
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
+
+}
