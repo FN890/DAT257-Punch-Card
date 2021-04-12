59ecc46db2723f3a16ebdae11eb1a56d9405f547
RasOtte <Rasmus.otterlind@gmail.com>
2021-04-12 12:23:51 +0200

    testing gitignore

--- src/main/java/com/punchcard/bookingsystem/BookingSystemApplication.java
+++ src/main/java/com/punchcard/bookingsystem/BookingSystemApplication.java
@@ -3,6 +3,8 @@ package com.punchcard.bookingsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+ // test
 @SpringBootApplication
 public class BookingSystemApplication {
 
