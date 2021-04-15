a6688eed212e56807f82298b931cd310b3733b79
Sara Arnesen <saraarnesen00@gmail.com>
2021-04-15 11:26:58 +0200

    First version of activity created by Sara & Victoria

--- src/main/java/com/punchcard/bookingsystem/repositories/ActivityRepository.java
+++ src/main/java/com/punchcard/bookingsystem/repositories/ActivityRepository.java
@@ -1,7 +1,9 @@
 package com.punchcard.bookingsystem.repositories;
 
+import com.punchcard.bookingsystem.tables.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//Extends JpaRepository
-public interface ActivityRepository {
+@Repository
+public interface ActivityRepository  extends JpaRepository<Activity, String> {
 }
--- src/main/java/com/punchcard/bookingsystem/tables/Activity.java
+++ src/main/java/com/punchcard/bookingsystem/tables/Activity.java
@@ -1,11 +1,46 @@
 package com.punchcard.bookingsystem.tables;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-//Commented so class isn't marked as having errors
-//@Entity
+@Entity
 @Table
 public class Activity {
 
+    @Id
+    private String name ;
+    private Integer price ;
+    private Integer maxSize ;
+
+    public Activity() {
+    }
+
+    public Activity(String name, Integer price, Integer maxSize) {
+        this.name = name ;
+        this.price = price ;
+        this.maxSize = maxSize ;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    public Integer getMaxSize() {
+        return maxSize ;
+    }
+    public void setMaxSize() {
+        this.maxSize = maxSize ;
+    }
 }
+
