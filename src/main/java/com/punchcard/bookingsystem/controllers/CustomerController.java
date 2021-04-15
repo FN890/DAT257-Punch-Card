8c76104db9f792d94ffc56f121d388a4982dc043
Bamsefar <bojkat@hotmail.com>
2021-04-15 10:52:52 +0200

    Added first implementation of Customer

--- src/main/java/com/punchcard/bookingsystem/controllers/CustomerController.java
+++ src/main/java/com/punchcard/bookingsystem/controllers/CustomerController.java
@@ -1,11 +1,17 @@
 package com.punchcard.bookingsystem.controllers;
 
 import com.punchcard.bookingsystem.services.CustomerService;
+import com.punchcard.bookingsystem.tables.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
+@RequestMapping(path = "api/v1/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -14,4 +20,10 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
 }
--- src/main/java/com/punchcard/bookingsystem/tables/Customer.java
+++ src/main/java/com/punchcard/bookingsystem/tables/Customer.java
@@ -1,10 +1,39 @@
 package com.punchcard.bookingsystem.tables;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Entity
+@Entity
 @Table
 public class Customer {
 
+    @Id
+    private String phoneNr;
+    private String name;
+
+
+    public Customer() {
+    }
+
+    public Customer(String phoneNr, String name) {
+        this.phoneNr = phoneNr;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
 }
