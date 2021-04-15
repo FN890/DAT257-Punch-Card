203912377f202ca039ea4eb0fb37794d24889eb2
RasOtte <Rasmus.otterlind@gmail.com>
2021-04-15 11:31:45 +0200

    Changed buttons for navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,6 @@ import {
 import ExampleRouter from './example-router/ExampleRouter'
 import Bookings from "./components/bookings/Bookings";
 
-const bookings = 'Här kommer bokningar att visas!'
 const newBooking = 'Här kommer man kunna skapa nya bokningar!'
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
     <Router>
       <Switch>
         <Route path="/bokningar">
-          <ExampleRouter title={bookings} />
           <Bookings/>
         </Route>
         <Route path='/boka'>
