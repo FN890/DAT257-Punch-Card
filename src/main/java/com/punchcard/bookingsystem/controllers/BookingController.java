487de02a1bd48609bc3294ed261ee74a6af3ed01
Gustav Wadström <gustav.wadstrm@gmail.com>
2021-04-15 11:27:22 +0200

    Added API path for booking

--- src/main/java/com/punchcard/bookingsystem/controllers/BookingController.java
+++ src/main/java/com/punchcard/bookingsystem/controllers/BookingController.java
@@ -3,9 +3,11 @@ package com.punchcard.bookingsystem.controllers;
 import com.punchcard.bookingsystem.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/booking")
 public class BookingController {
 
     private final BookingService bookingService;
