ec57c5388a883df7cf932d107d5acd5b1b579380
gustavwww <gustav.wadstrom@telia.com>
2021-04-16 11:59:46 +0200

    Fixed native query

--- src/main/java/com/punchcard/bookingsystem/repositories/BookingRepository.java
+++ src/main/java/com/punchcard/bookingsystem/repositories/BookingRepository.java
@@ -11,10 +11,10 @@ import java.util.Optional;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    @Query(value = "SELECT * FROM Booking WHERE customerPhone = ?1")
+    @Query(value = "SELECT * FROM Booking WHERE customerPhone = ?1", nativeQuery = true)
     List<Booking> findByCustomerPhone(int phone);
 
-    @Query(value = "SELECT * FROM Booking WHERE id = ?1")
+    @Query(value = "SELECT * FROM Booking WHERE id = ?1", nativeQuery = true)
     Optional<Booking> findById(int id);
 
 }
