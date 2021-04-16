0d1cc29f308917d57afbfd937387794379acfca2
Bamsefar <bojkat@hotmail.com>
2021-04-16 13:01:52 +0200

    Added id for Activity and POST method for Activity

--- src/main/java/com/punchcard/bookingsystem/repositories/ActivityRepository.java
+++ src/main/java/com/punchcard/bookingsystem/repositories/ActivityRepository.java
@@ -4,6 +4,11 @@ import com.punchcard.bookingsystem.tables.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ActivityRepository  extends JpaRepository<Activity, String> {
+public interface ActivityRepository  extends JpaRepository<Activity, Long> {
+
+    //SELECT * FROM Activity WHERE name = ?
+    Optional<Activity> findActivityByName(String name);
 }
