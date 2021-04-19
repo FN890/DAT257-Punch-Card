e2640abf9d369a281e3eac84234fcf3a50f2b604
gustavwww <gustav.wadstrom@telia.com>
2021-04-19 23:22:29 +0200

    Removed customer cascade

--- src/main/java/com/punchcard/bookingsystem/tables/Customer.java
+++ src/main/java/com/punchcard/bookingsystem/tables/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
     private String phoneNr;
     private String name;
 
-    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
     private List<Booking> bookings;
 
     public Customer() {}
