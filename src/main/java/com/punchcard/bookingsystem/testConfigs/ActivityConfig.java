4ad298a2bca5015c62a191452e12fde3a473ce77
Bamsefar <bojkat@hotmail.com>
2021-04-20 10:15:53 +0200

    Added config for activities

---
+++ src/main/java/com/punchcard/bookingsystem/testConfigs/ActivityConfig.java
@@ -0,0 +1,23 @@
+package com.punchcard.bookingsystem.testConfigs;
+
+import com.punchcard.bookingsystem.repositories.ActivityRepository;
+import com.punchcard.bookingsystem.tables.Activity;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ActivityConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(ActivityRepository activityRepository) {
+        return args -> {
+            Activity badtunna = new Activity("Badtunna", 1200, 7);
+            Activity bastu = new Activity("Bastu", 500, 7);
+
+            activityRepository.saveAll(List.of(badtunna, bastu));
+        };
+    }
+}
