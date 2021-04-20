047ce404bade92c612b93a86d6ff1ed0f3d752d4
Sara Arnesen <saraarnesen00@gmail.com>
2021-04-20 12:22:30 +0200

    Removed ReactBootStrap

--- frontend/src/components/prices/ActivityTable.js
+++ frontend/src/components/prices/ActivityTable.js
@@ -1,42 +1,27 @@
 import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import ActivityItem from "./ActivityItem";
-import * as ReactBootStrap from "react-bootstrap"
 
 const ActivityTable = () => {
-    const activities = [
-        {name: "Stugan", price:"100kr/person och natt"},
-        {name: "Grill", price:"Gratis :D"},
-        {name: "Badtunna", price:"1200 kr"},
-        {name: "Bastu", price:"500 kr"},
-        {name: "Vattenskidåkning", price:"Vet ej"},
-        {name: "Svensex/Möhippa", price:"Mycket"},
-    ]
+    /**
+    const [activity, setActivity] = useState([]);
+    const activityService = new ActivityItem();
 
-    const renderActivities = (activity, index) => {
-        return(
+    useEffect(() => {
+        activityService.getActivityService().then(data => setActivity(data));
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-            <tr key = {index}>
-                <td>{activity.name}</td>
-                <td>{activity.price}</td>
-            </tr>
-        )
-    }
     return (
         <div>
-            <h1 id='title'>Priser</h1>
-
-            <ReactBootStrap.Table striped bordered hover>
-                <thead>
-                <tr>
-                    <th>Aktivitetens namn</th>
-                    <th>Pris</th>
-                </tr>
-                </thead>
-                <tbody>
-                {activities.map(renderActivities)}
-                </tbody>
-            </ReactBootStrap.Table>
+            <div className="card">
+                <DataTable value={products}>
+                    <Column field="name" header="Aktivitetens namn"></Column>
+                    <Column field="price" header="Pris"></Column>
+                </DataTable>
+            </div>
         </div>
-    )
+    );
+     */
 }
-export default ActivityTable ;
