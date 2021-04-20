58bd1346a81e5d9014d813cc831e2413a9c8a7dc
RasOtte <Rasmus.otterlind@gmail.com>
2021-04-20 13:00:49 +0200

    BookingService is now supposed to return an array of jsondata

--- frontend/src/components/bookings/BookingService.js
+++ frontend/src/components/bookings/BookingService.js
@@ -1,20 +1,32 @@
 import axios from 'axios';
 
-export default function BookingService(){
-
+export default function BookingService() {
+    /**
+     *  Used to restructure json data into a usable format for the calendar
+     */
     return axios.get('/api/v1/booking').then(resp => {
         if (resp.status === 200) {
-            console.log(resp.data)
-            let parsedData = (resp.data).map(
+            (resp.data).map(
                 obj => {
+                    let i;
+                    var bookingsArray = [obj.reservations.length]
+                    for (i = 0; i < obj.reservations.length; i++) {
+                        bookingsArray[i] = {
+                            "id" : obj.reservations[i].id,
+                            "title": obj.reservations[i].activity.name,
+                            "start": obj.reservations[i].startTime,
+                            "end": obj.reservations[i].endTime,
+
+                        }
+                    }
+                    console.log(bookingsArray)
                     return {
-                        "title": obj.name,
-                        "start": obj.startTime,
-                        "end": obj.endTime,
+                        bookingsArray
                     }
                 }
             )
-            return parsedData;
+
+
         }
     });
 }
