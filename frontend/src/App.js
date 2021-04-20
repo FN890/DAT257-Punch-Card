1ac4e38c9bafdbcdd83c4e1550b90145db973248
Bamsefar <bojkat@hotmail.com>
2021-04-20 10:42:52 +0200

    Added Prices function to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ExampleRouter from './example-router/ExampleRouter'
 import Bookings from "./components/bookings/Bookings";
 import NavBar from "./components/navbar/Navbar"
 import NewBooking from "./components/new_booking/NewBooking"
+import Prices from "./components/prices/Prices";
 
 const newBooking = 'Här kommer man kunna skapa nya bokningar!'
 const prices = 'Priser'
@@ -26,7 +27,7 @@ function App() {
         </Route>
         <Route path='/priser'>
           <NavBar />
-          <ExampleRouter title={prices} />
+          <Prices />
         </Route>
         <NavBar />
         <Route path='/installningar'>
--- frontend/src/components/prices/Prices.js
+++ frontend/src/components/prices/Prices.js
@@ -1,2 +1,8 @@
 import React from "react";
 import axios from "axios";
+
+export default function Prices() {
+    return (
+        <h1>Hejsan svejsan</h1>
+    )
+}
