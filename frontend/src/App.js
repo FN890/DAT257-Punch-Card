34933544f7dc1ea0dacc490d2c293307bcaf655e
FN <fn_890@icloud.com>
2021-04-20 12:52:59 +0200

    Added UI elements to new booking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
         </Route>
         <Route path='/nybokning'>
           <NavBar />
+          <NewBooking/>
         </Route>
         <Route path='/priser'>
           <NavBar />
--- frontend/src/components/new_booking/NewBooking.js
+++ frontend/src/components/new_booking/NewBooking.js
@@ -1,11 +1,35 @@
 import CustomerInfo from "./components/CustomerInfo"
+import 'primeflex/primeflex.css';
+import React, { useState } from 'react';
+
+import { InputText } from 'primereact/inputtext';
+import { InputMask } from 'primereact/inputmask';
+import { Button } from 'primereact/button'
 
 export default function NewBooking() {
+
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+
     return (
         <div>
-            <h1>
-                Hello
-            </h1>
+            <div className="card">
+                <div className="p-fluid p-formgrid p-grid p-m-5">
+                    <div className="p-field p-col-12 p-md-4">
+                        <span className="p-float-label">
+                            <InputText id="inputtext" value={name} onChange={(e) => setName(e.target.value)} />
+                            <label htmlFor="inputtext">Namn</label>
+                        </span>
+                    </div>
+                    <div className="p-field p-col-12 p-md-4">
+                        <span className="p-float-label">
+                            <InputMask id="inputmask" value={phone} onChange={(e) => setPhone(e.value)} mask="9999-99 99 99" />
+                            <label htmlFor="inputmask">Telefon</label>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
