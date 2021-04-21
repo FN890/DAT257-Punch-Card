7e0eb60e2b056f8e1946995f60b2d0e4db6edfa5
FN <fn_890@icloud.com>
2021-04-21 19:24:06 +0200

    Improved layout

--- frontend/src/components/new_booking/components/CustomerInfo.js
+++ frontend/src/components/new_booking/components/CustomerInfo.js
@@ -20,41 +20,39 @@ export default function CustomerInfo() {
 
     return (
         <div className="p-grid p-dir-col">
-            <div className="p-col-0 p-mx-5">
-                <div className="p-d-flex p-mx-2">
+                <div className="p-d-flex p-mx-5 p-mb-3">
                     <h1>Skapa ny bokning</h1>
                 </div>
-            </div>
-            <div className="p-col-1 p-mx-5 p-mb-3">
-                <span className="p-float-label">
-                    <InputText id="name" value={responsible} onChange={(e) => setResponsible(e.target.value)} />
-                    <label htmlFor="name">Ansvarig</label>
-                </span>
-            </div>
-            <div className="p-col-1 p-mx-5 p-mb-3">
-                <span className="p-float-label">
-                    <InputText id="name" value={customer} onChange={(e) => setCustomer(e.target.value)} />
-                    <label htmlFor="name">Kundnamn</label>
-                </span>
-            </div>
-            <div className="p-col-1 p-mx-5 p-mb-3">
-                <span className="p-float-label">
-                    <InputMask mask="9999-99 99 99" id="inputPhone" value={phone} onChange={(e) => setPhone(e.value)} useGrouping={false} format={false} />
-                    <label htmlFor="inputPhone">Telefon</label>
-                </span>
-            </div>
-            <div className="p-col-1 p-mx-5 p-mb-3">
-                <span className="p-float-label">
-                    <InputNumber id="inputGroupSize" value={numberOfStudent} onChange={(e) => setNumberOfStudent(e.value)} min={0} max={100} />
-                    <label htmlFor="inputGroupSize">Gruppstorlek</label>
-                </span>
-            </div>
-            <div className="p-col-1 p-mx-5">
-                <span className="p-float-label">
-                    <InputTextarea id="textarea" value={notes} onChange={(e) => setNotes(e.value)} rows={3} autoResize />
-                    <label htmlFor="textarea">Anteckningar</label>
-                </span>
-            </div>
+                <div className="p-d-flex p-mx-5 p-mb-3">
+                    <span className="p-float-label">
+                        <InputText id="name" value={responsible} onChange={(e) => setResponsible(e.target.value)} />
+                        <label htmlFor="name">Ansvarig</label>
+                    </span>
+                </div>  
+                <div className="p-d-flex p-mx-5 p-mb-3">
+                    <span className="p-float-label">
+                        <InputText id="name" value={customer} onChange={(e) => setCustomer(e.target.value)} />
+                        <label htmlFor="name">Kundnamn</label>
+                    </span>
+                </div>  
+                <div className="p-d-flex p-mx-5 p-mb-3">
+                    <span className="p-float-label">
+                        <InputMask mask="9999-99 99 99" id="inputPhone" value={phone} onChange={(e) => setPhone(e.value)} useGrouping={false} format={false} />
+                        <label htmlFor="inputPhone">Telefon</label>
+                    </span>
+                </div>
+                <div className="p-d-flex p-mx-5 p-mb-3">
+                    <span className="p-float-label">
+                        <InputNumber id="inputGroupSize" value={numberOfStudent} onChange={(e) => setNumberOfStudent(e.value)} min={0} max={100} />
+                        <label htmlFor="inputGroupSize">Gruppstorlek</label>
+                    </span>
+                </div>     
+                <div className="p-d-flex p-mx-5 p-mb-3">
+                    <span className="p-float-label">
+                        <InputTextarea id="textarea" value={notes} onChange={(e) => setNotes(e.value)} rows={3} autoResize />
+                        <label htmlFor="textarea">Anteckningar</label>
+                    </span>
+                </div>       
         </div>
     )
 }
--- frontend/src/components/new_booking/components/FinishButtonGroup.js
+++ frontend/src/components/new_booking/components/FinishButtonGroup.js
@@ -3,15 +3,9 @@ import 'primeflex/primeflex.css';
 
 export default function FinishButtonGroup() {
     return (
-        <div className="p-grid p-justify-end">
-            <div className="p-col">
-                <div className="box">
-                    <div className="p-d-flex p-jc-end p-mr-5">
-                        <Button label="Avbryt" className="p-button-danger p-button-text p-mr-2" />
-                        <Button label="Skapa bokning" className="p-button-raised" />
-                    </div>
-                </div>
-            </div>
+        <div className="p-grid p-justify-end p-m-3">
+                <Button label="Avbryt" className="p-button-danger p-button-text p-mr-2" />
+                <Button label="Skapa bokning" className="p-button-raised" />
         </div>
     )
 }
