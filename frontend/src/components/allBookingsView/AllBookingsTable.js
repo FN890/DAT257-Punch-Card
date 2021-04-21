79ce4de4c28d055adf1bfbcb8470575eca84e710
RasOtte <Rasmus.otterlind@gmail.com>
2021-04-21 16:21:02 +0200

    Bookings now populate the allbookings view

--- frontend/src/components/allBookingsView/AllBookingsTable.js
+++ frontend/src/components/allBookingsView/AllBookingsTable.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
 import AllBookingsService from "./AllBookingsService";
+import {Toast} from 'primereact/toast';
 
 export default function AllBookingsTable() {
     const [booking, setBookings] = useState([]);
@@ -9,19 +10,49 @@ export default function AllBookingsTable() {
 
     useEffect(() => {
         allBookingsService.getAllBookings().then(data => setBookings(data));
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    }, []);
+
+
+    for (let i = 0; i < booking.length; i++) {
+        booking[i].startTime = booking[i].startTime.replaceAll('T', ' ');
+        booking[i].endTime = booking[i].endTime.replaceAll('T', ' ');
+        booking[i].startTime = booking[i].startTime.slice(0, 16)
+        booking[i].endTime = booking[i].endTime.slice(0, 16)
+    }
+    const toast = useRef(null);
+
+    const onRowSelect = (event) => {
+        toast.current.show({
+            severity: 'warn',
+            summary: 'Bokning vald',
+            detail: 'place holder event will change to selected booking '+ event.data.responsible,
+            life: 3000
+        });
+    }
+
+    const [selectedBooking, setSelectedBooking] = useState(null);
 
     return (
-        <div>
+        <div className="p-shadow-5 p-m-5">
+
+
+            <Toast ref={toast} />
+
             <div className="card">
-                <DataTable value={booking}>
-                    <Column field="id" header="boknings-Id"></Column>
+                <DataTable value={booking} selection={selectedBooking}
+                           onSelectionChange={e => setSelectedBooking(e.value)} selectionMode="single" dataKey="id"
+                           onRowSelect={onRowSelect}>
+                    <Column field="responsible" header="Namn"></Column>
+                    <Column field="customer.phoneNr" header="Telefon"></Column>
+                    <Column field="id" header="Boknings-Id"></Column>
                     <Column field="groupSize" header="Antal personer"></Column>
-
-
-
+                    <Column field="startTime" header="Start datum "></Column>
+                    <Column field="endTime" header="Slut datum"></Column>
+                    <Column field="description" header="Övrigt"></Column>
                 </DataTable>
             </div>
         </div>
     );
 }
+
+
