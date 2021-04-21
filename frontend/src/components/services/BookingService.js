fe68333707857e0a86c7a793040eb921198cf68c
Simon Andersson <xerxz5115@gmail.com>
2021-04-21 17:59:30 +0200

    Updated documentation in BookingService

--- frontend/src/components/services/BookingService.js
+++ frontend/src/components/services/BookingService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function BookingService() {
     /**
-     *  Used to restructure json data into a usable format for the calendar
+     *  GET request to get JSON of all bookings
      */
     return axios.get('/api/v1/booking').then(resp => resp.data);
 }
