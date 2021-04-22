b07b8fc70f41c05423db501b072a86e22ecde497
gustavwww <gustav.wadstrom@telia.com>
2021-04-22 23:19:35 +0200

    Added basic info

--- frontend/src/components/individual_booking/IndividualBooking.js
+++ frontend/src/components/individual_booking/IndividualBooking.js
@@ -2,6 +2,8 @@ import _default from '@fullcalendar/daygrid';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import BookingService from '../services/BookingService'
+import 'primeflex/primeflex.css';
+import './IndividualBooking.css';
 
 export default function IndividualBooking() {
     const { id } = useParams();
@@ -9,15 +11,39 @@ export default function IndividualBooking() {
 
     useEffect(() => {
         new BookingService().getIndividualBooking(id).then(data => setBooking(data));
-        console.log(booking);
     });
     
+
     //TODO: Return invalid id when invalid.
 
     return(
     <div>
-        <h1>Got the id: {id}</h1>
-        <h2>Responsible: {booking.responsible}</h2>
+        <div className="p-shadow-5 p-m-5">
+        <h1>Bokning #{id}</h1>
+            <div>
+                <h3 className="form">Beskrivning: </h3>
+                <h4 className="value">{booking.description}</h4>
+            </div>
+            <div>
+                <h3 className="form">Starttid: </h3>
+                <h4 className="value">{booking.startTime}</h4>
+            </div>
+            <div>
+                <h3 className="form">Sluttid: </h3>
+                <h4 className="value">{booking.endTime}</h4>
+            </div>
+            <div>
+                <h3 className="form">Ansvarig: </h3>
+                <h4 className="value">{booking.responsible}</h4>
+            </div>
+            <div>
+                <h3 className="form">Grupp storlek: </h3>
+                <h4 className="value">{booking.groupSize}</h4>
+            </div>
+            
+            
+        </div>
     </div>
+
     );
 }
