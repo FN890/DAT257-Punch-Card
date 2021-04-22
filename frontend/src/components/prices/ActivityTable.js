198ac54d0d497c1440775ee36fb5e5890fba9ed9
Sara Arnesen <saraarnesen00@gmail.com>
2021-04-22 12:13:37 +0200

    Added some CSS and filter feature to the price-table

--- frontend/src/components/prices/ActivityTable.js
+++ frontend/src/components/prices/ActivityTable.js
@@ -3,25 +3,41 @@ import React, { useState, useEffect } from 'react';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import ActivityService from '../services/ActivityService';
+import './ActivityTable.css';
+
 
 export default function ActivityTable()  {
     const [activity, setActivity] = useState([]);
+    const [multiSortMeta, setMultiSortMeta] = useState([{ field: 'category', order: -1 }]);
     const activityService = new ActivityService();
+
+    const formatCurrency = (value) => {
+        return value.toLocaleString('sv-SE', {style: 'currency', currency: 'SEK'});
+    }
+
+    const priceBodyTemplate = (rowData) => {
+        return formatCurrency(rowData.price);
+    }
 
     useEffect(() => {
         activityService.getActivities().then(data => setActivity(data));
         }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+    const header = (
+        <div className="table-header">
+            Priser för aktiviteter
+        </div>
+    );
+    const footer = `Det finns totalt ${activity ? activity.length : 0} aktiviteter tillagda.`;
+
     return (
-        <div>
-            <div className="card">
-                <DataTable value={activity}>
-                    <Column field="name" header="Aktivitetens namn"></Column>
-                    <Column field="price" header="Pris"></Column>
-                    <Column field="maxSize" header="Max antal"></Column>
+            <div className="p-shadow-5 p-m-5">
+                <DataTable value={activity} header={header} footer={footer}>
+                    <Column field="name" header="Aktivitetens namn" sortable></Column>
+                    <Column field="price" header="Pris"  body={priceBodyTemplate} sortable></Column>
+                    <Column field="maxSize" header="Max antal" sortable></Column>
                 </DataTable>
             </div>
-        </div>
     );
 
 }
--- frontend/src/components/services/ActivityService.js
+++ frontend/src/components/services/ActivityService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default class ActivityService {
 
     getActivities() {
-
         return axios.get('/api/v1/activity').then(resp => {
             return resp.data
         });
