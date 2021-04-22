01e29e67e6ec9ccb5c3cf8b30337b59d657e0754
gustavwww <gustav.wadstrom@telia.com>
2021-04-22 16:41:18 +0200

    Added simple return

--- frontend/src/components/individual_booking/IndividualBooking.js
+++ frontend/src/components/individual_booking/IndividualBooking.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import _default from '@fullcalendar/daygrid';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import BookingService from '../services/BookingService'
 
 export default function IndividualBooking() {
     const { id } = useParams();
+    const [booking, setBooking] = useState({});
 
+    useEffect(() => {
+        new BookingService().getIndividualBooking(id).then(data => setBooking(data));
+        console.log(booking);
+    });
+    
+    //TODO: Return invalid id when invalid.
 
     return(
     <div>
         <h1>Got the id: {id}</h1>
+        <h2>Responsible: {booking.responsible}</h2>
     </div>
     );
 }
