c36e0566eaa7332a4162c9f715aa2ea85c9f4801
RasOtte <Rasmus.otterlind@gmail.com>
2021-04-23 13:33:14 +0200

    added navigation to individual bookings from allbookings

--- frontend/src/components/allBookingsView/AllBookingsTable.js
+++ frontend/src/components/allBookingsView/AllBookingsTable.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useRef} from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
 import BookingService from "../services/BookingService";
-import {Toast} from 'primereact/toast';
+import {useHistory, useLocation} from "react-router-dom";
 
 /**
  * Creates the table that shows all bookings with customer info
@@ -23,35 +23,34 @@ export default function AllBookingsTable() {
         booking[i].startTime = booking[i].startTime.slice(0, 16)
         booking[i].endTime = booking[i].endTime.slice(0, 16)
     }
-    const toast = useRef(null);
+
+    const history = useHistory();
 
     const onRowSelect = (event) => {
-        toast.current.show({
-            severity: 'warn',
-            summary: 'Bokning vald',
-            detail: 'place holder event will change to selected booking '+ event.data.customer.name,
-            life: 3000
-        });
+
+        history.push("/allabokningar/" + event.data.id);
+
     }
 
+
     const [selectedBooking, setSelectedBooking] = useState(null);
-    const [multiSortMeta, setMultiSortMeta] = useState([{ field: 'category', order: -1 }]);
+    const [multiSortMeta, setMultiSortMeta] = useState([{field: 'category', order: -1}]);
 
     return (
         <div className="p-shadow-5 p-m-5">
-            <Toast ref={toast} />
-                <DataTable value={booking} selection={selectedBooking}
-                           onSelectionChange={e => setSelectedBooking(e.value)} selectionMode="single" dataKey="id"
-                           onRowSelect={onRowSelect}>
-                    <Column field="customer.name" header="Namn"sortable> </Column>
-                    <Column field="customer.phoneNr" header="Telefon"></Column>
-                    <Column field="id" header="Boknings-Id"sortable></Column>
-                    <Column field="groupSize" header="Antal personer"sortable></Column>
-                    <Column field="startTime" header="Start datum "sortable></Column>
-                    <Column field="endTime" header="Slut datum"sortable></Column>
-                    <Column field="description" header="Övrigt"></Column>
-                    <Column field="responsible" header="Skapad av" sortable></Column>
-                </DataTable>
+
+            <DataTable value={booking} selection={selectedBooking}
+                       onSelectionChange={e => setSelectedBooking(e.value)} selectionMode="single" dataKey="id"
+                       onRowSelect={onRowSelect}>
+                <Column field="customer.name" header="Namn" sortable></Column>
+                <Column field="customer.phoneNr" header="Telefon"></Column>
+                <Column field="id" header="Boknings-Id" sortable></Column>
+                <Column field="groupSize" header="Antal personer" sortable></Column>
+                <Column field="startTime" header="Start datum " sortable></Column>
+                <Column field="endTime" header="Slut datum" sortable></Column>
+                <Column field="description" header="Övrigt"></Column>
+                <Column field="responsible" header="Skapad av" sortable></Column>
+            </DataTable>
         </div>
     );
 }
