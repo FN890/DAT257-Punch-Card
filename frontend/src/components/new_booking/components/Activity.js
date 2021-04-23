65f7e9b835a0d073efc221a316b95094c13d8a88
FN <fn_890@icloud.com>
2021-04-23 12:04:39 +0200

    Fixed error with focusedInput

--- frontend/src/components/new_booking/components/Activity.js
+++ frontend/src/components/new_booking/components/Activity.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import 'react-dates/initialize';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController, FocusedInputShape } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import { Dropdown } from 'primereact/dropdown';
 import { Button } from 'primereact/button';
 import 'primeflex/primeflex.css';
+import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment'
 
 export default function Activity() {
 
     const [activity, setActivity] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [focused, setFocused] = useState('');
+    const [startDate, setStartDate] = useState(moment());
+    const [endDate, setEndDate] = useState(moment());
+    const [focused, setFocused] = useState(null);
+
+    const startDateId = uuidv4();
+    const endDateId = uuidv4();
 
     return (
         <div className="p-fluid p-ai-center p-mx-5 p-mb-5">
@@ -24,16 +29,16 @@ export default function Activity() {
             <div className="p-mb-2">
                 <DateRangePicker
                     startDate={startDate}
-                    startDateId="start_date_id"
+                    startDateId={startDateId}
                     endDate={endDate}
-                    endDateId="end_date_id"
+                    endDateId={endDateId}
                     onDatesChange={({ startDate, endDate }) => {
                         setStartDate(startDate);
                         setEndDate(endDate);
                     }}
                     focusedInput={focused}
                     onFocusChange={focusedInput => {
-                        setFocused(focusedInput);
+                        setFocused({ focusedInput });
                     }}
                 />
             </div>
