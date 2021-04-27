b104243a3927165f76ecae081e04308e0f4edc77
FN <fn_890@icloud.com>
2021-04-27 16:44:55 +0200

    Added date and time picker

--- frontend/src/components/new-booking/components/Activity.js
+++ frontend/src/components/new-booking/components/Activity.js
@@ -7,31 +7,35 @@ import { Button } from 'primereact/button';
 import 'primeflex/primeflex.css';
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+var daily = true;
 
 export default function Activity(props) {
 
-    const activityNames = props.activityNames;
+    const activityInfo = props.activityInfo;
 
     const [activity, setActivity] = useState(null);
     const [startDate, setStartDate] = useState(moment());
     const [endDate, setEndDate] = useState(moment());
     const [focused, setFocused] = useState(null);
+    const [minutes, setMinutes] = useState();
+    const [hours, setHours] = useState();
+    const [startDateTime, setStartDateTime] = useState(new Date());
+
 
     const startDateId = uuidv4();
     const endDateId = uuidv4();
 
-    return (
-        <div className="p-fluid p-ai-center p-mx-5 p-mb-5">
-            <div className="p-d-flex p-my-1 ">
-                <span className="p-float-label" style={{width:'85%'}}>
-                    <Dropdown id="dropdown" optionLabel="name" options={activityNames} value={activity} onChange={(e) => setActivity(e.value)} />
-                    <label htmlFor="dropdown">Aktivitet</label>
-                </span>
-                <div className="p-ml-auto">
-                    <Button className="p-button-raised p-button-danger" icon="pi pi-trash" iconPos="right" />
-                </div>
-            </div>
-            <div className="p-mb-2">
+    const setSelectedActivity = (value) => {
+        daily = value.daily;
+        setActivity(value);
+    }
+
+    const getDateSelect = () => {
+        if (daily === false) {
+            return (
                 <DateRangePicker
                     startDate={startDate}
                     startDateId={startDateId}
@@ -48,9 +52,36 @@ export default function Activity(props) {
                     }}
                     numberOfMonths={1}
                 />
+            )
+        } else if (daily === true) {
+            return (
+                <DatePicker
+                    selected={startDateTime}
+                    onChange={date => setStartDateTime(date)}
+                    showTimeSelect
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                />
+            )
+        }
+    }
+
+    return (
+        <div className="p-fluid p-ai-center p-mx-5 p-mb-5">
+            <div className="p-d-flex p-my-1 ">
+                <span className="p-float-label" style={{ width: '85%' }}>
+                    <Dropdown id="dropdown" optionLabel="name" options={activityInfo} value={activity} onChange={(e) => setSelectedActivity(e.value)} />
+                    <label htmlFor="dropdown">Aktivitet</label>
+                </span>
+                <div className="p-ml-auto">
+                    <Button className="p-button-raised p-button-danger" icon="pi pi-trash" iconPos="right" />
+                </div>
+            </div>
+            <div className="p-mb-2">
+                {getDateSelect()}
             </div>
 
         </div>
 
     )
+
 }
