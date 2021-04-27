1d022ebebed96c1d51667c7540d44457d355fb58
gustavwww <gustav.wadstrom@telia.com>
2021-04-27 15:24:40 +0200

    Fixed query error

--- src/main/java/com/punchcard/bookingsystem/repositories/ReservationRepository.java
+++ src/main/java/com/punchcard/bookingsystem/repositories/ReservationRepository.java
@@ -16,7 +16,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query(value = "SELECT * FROM Reservation WHERE (:fromDate BETWEEN start_time AND end_time) OR (:toDate BETWEEN start_time AND end_time) OR (:fromDate <= start_time AND :toDate >= end_time)", nativeQuery = true)
     List<Reservation> findBetweenDates(@Param("fromDate") LocalDateTime fromDate, @Param("toDate") LocalDateTime toDate);
 
-    @Query(value = "SELECT * FROM Reservation WHERE activity_name = :activityName AND ((:fromDate BETWEEN start_time AND end_time) OR (:toDate BETWEEN start_time AND end_time) OR (:fromDate <= start_time AND :toDate >= end_time))", nativeQuery = true)
+    @Query(value = "SELECT * FROM Reservation WHERE activity_name = :activityName AND ((:startTime BETWEEN start_time AND end_time) OR (:endTime BETWEEN start_time AND end_time) OR (:startTime <= start_time AND :endTime >= end_time))", nativeQuery = true)
     Optional<Reservation> findOccupied(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime, @Param("activityName") String activityName);
 
 }
