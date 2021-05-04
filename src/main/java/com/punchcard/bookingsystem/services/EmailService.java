741b7a743d9624993f6269fc03134ed0d9c6b722
Bamsefar <bojkat@hotmail.com>
2021-05-04 11:03:51 +0200

    Added EmailService and updated maven dependencies

---
+++ src/main/java/com/punchcard/bookingsystem/services/EmailService.java
@@ -0,0 +1,32 @@
+package com.punchcard.bookingsystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class EmailService {
+
+    private final String mail = "alingsasvatten@gmail.com";
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String to) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(mail);
+        message.setTo(to);
+        message.setSubject("Bokningsbekräftelse Alingsås Vattenskidklubb");
+        message.setText("Hejsan, gillar du också hollandaisesås?");
+
+        javaMailSender.send(message);
+    }
+}
