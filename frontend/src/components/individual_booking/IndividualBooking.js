6ec20290dbf392e67dcad2ce5e78c95d5e454f2f
Sara Arnesen <saraarnesen00@gmail.com>
2021-05-11 11:37:10 +0200

    Viktor and I fixed the individual booking by making it editable

--- frontend/src/components/individual_booking/IndividualBooking.js
+++ frontend/src/components/individual_booking/IndividualBooking.js
@@ -1,28 +1,45 @@
 import React, {useState, useEffect, useRef} from 'react';
-import { useParams } from 'react-router';
+import {useParams} from 'react-router';
 import 'primeflex/primeflex.css';
 import './IndividualBooking.css';
-import { Button } from 'primereact/button';
+import {Button} from 'primereact/button';
 import BookingService from '../services/BookingService';
 import Reservation from './components/Reservation';
-import { Dialog } from 'primereact/dialog';
+import {Dialog} from 'primereact/dialog';
 import {useHistory} from 'react-router-dom';
+import {InputText} from "primereact/inputtext";
+import {ToggleButton} from "primereact/togglebutton";
 
 
 export default function IndividualBooking() {
-    const { id } = useParams();
+    const {id} = useParams();
     const [booking, setBooking] = useState({});
     const [reservations, setReservations] = useState([]);
     const [customer, setCustomer] = useState({})
     const [deleteBookingDialog, setDeleteBookingDialog] = useState(false);
     const history = useHistory();
 
+    const [responsible, setResponsible] = useState('');
+    const [isPaid, setPaid] = useState('');
+    const [price, setPrice] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [description, setDescription] = useState('');
+    const [editable, setEditable] = useState(true);
+
+
     const bookingService = new BookingService();
 
     useEffect(() => {
         bookingService.getIndividualBooking(id).then(data => {
             setBooking(data);
-            setCustomer(data.customer);
+            setCustomer(data.customer.name);
+            setResponsible(data.responsible);
+            setPaid(data.paid);
+            setPrice(data.price);
+            setEmail(data.customer.email);
+            setPhone(data.customer.phoneNr);
+            setDescription(data.description);
             const res = [];
             data.reservations.forEach(r => {
                 res.push(
@@ -50,62 +67,86 @@ export default function IndividualBooking() {
 
     const deleteBookingDialogFooter = (
         <React.Fragment>
-            <Button label="Nej" icon="pi pi-times" className="p-button-text" onClick={hideDeleteBookingDialog} />
-            <Button label="Ja" icon="pi pi-check" className="p-button-text" onClick={deleteProduct} />
+            <Button label="Nej" icon="pi pi-times" className="p-button-text" onClick={hideDeleteBookingDialog}/>
+            <Button label="Ja" icon="pi pi-check" className="p-button-text" onClick={deleteProduct}/>
         </React.Fragment>
     );
 
-    return(
-    <div className="p-grid p-jc-center">
-        <div className="p-shadow-5 p-m-3 p-col-12" style={{width: "60%"}}>
-            <div className="p-grid p-jc-center" style={{margin: "30pt 0 0 0"}}>
-                <div className="p-card p-col-6" style={{width: "150pt", margin: "0 30pt"}}>
-                    <div className="p-m-2 p-text-left">
-                        <b>Kund:</b> {customer.name}
+    return (
+        <div className="p-grid p-jc-center">
+            <div className="p-shadow-5 p-m-3 p-col-12" style={{width: "60%"}}>
+                <div className="p-grid p-jc-center" style={{margin: "30pt 0 0 0"}}>
+                    <div className="p-card p-col-6" style={{width: "200pt", margin: "0 30pt"}}>
+                        <div className="p-m-2 p-text-left">
+                            <b>Kundens namn:</b>
+                            <InputText style={{margin: "5pt 0 0 0"}} disabled={editable} value={customer}
+                                       onChange={(e) => setCustomer(e.target.value)}/>
+                        </div>
+                        <div className="p-m-2 p-text-left">
+                            <b>Mobil:</b>
+                            <InputText style={{margin: "5pt 0 0 0"}} disabled={editable} value={phone}
+                                       onChange={(e) => setPhone(e.target.value)}/>
+                        </div>
+                        <div className="p-m-2 p-text-left">
+                            <b>E-mail:</b>
+                            <InputText style={{margin: "5pt 0 0 0"}} disabled={editable} value={email}
+                                       onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
                     </div>
-                    <div className="p-m-2 p-text-left">
-                        <b>Mobil:</b> {customer.phoneNr}
+                    <div className="p-card p-col-6" style={{width: "200pt", margin: "0pt 30pt"}}>
+                        <div className="p-m-2 p-text-left">
+                            <b>Boknings-id:</b> {booking.id}
+                        </div>
+                        <div className="p-m-2 p-text-left">
+                            <b>Ansvarig:</b>
+                            <InputText style={{margin: "5pt 0 0 0"}} disabled={editable} value={responsible}
+                                       onChange={(e) => setResponsible(e.target.value)}/>
+                        </div>
                     </div>
                 </div>
-                <div className="p-card p-col-6" style={{width: "150pt", margin: "0 30pt"}}>
-                    <div className="p-m-2 p-text-left">
-                        <b>Boknings-id:</b> {booking.id}
+                <div className="p-grid p-jc-center" style={{margin: "30pt 0 0 0"}}>
+                    <div className="p-col-6" style={{width: "150pt", margin: "0 30pt"}}>
+                        <div className="p-m-2 p-text-center">
+                            <b>Beskrivning:</b>
+                            <InputText style={{margin: "5pt 0 0 0"}} disabled={editable} value={description}
+                                       onChange={(e) => setDescription(e.target.value)}/>
+                        </div>
                     </div>
-                    <div className="p-m-2 p-text-left">
-                        <b>Ansvarig:</b> {booking.responsible}
+                    <div className="p-col-6" style={{width: "150pt", margin: "0 30pt"}}>
+                        <div className="p-text-center p-col-12">
+                            <b>Pris:</b>
+                            <InputText style={{margin: "5pt 0 0 0"}} disabled={editable} value={price}
+                                       onChange={(e) => setPrice(e.target.value)}/>
+                        </div>
+                        <div className="p-text-center p-col-12">
+                            <b>Betald:</b>
+                            <ToggleButton disabled={editable} style={{margin: "10pt 0 0 10pt"}} onLabel="Betalat"
+                                          offLabel="Ej betalt" onIcon="pi pi-check" offIcon="pi pi-times"
+                                          checked={isPaid} onChange={(e) => setPaid(e.target.value)}/>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="p-grid p-jc-center" style={{margin: "30pt 0 0 0"}}>
-            <div className="p-col-6" style={{width: "150pt", margin: "0 30pt"}}>
-                    <div className="p-m-2 p-text-center">
-                        <b>Beskrivning:</b> {booking.description}
-                    </div>
-                </div>
-            <div className="p-col-6" style={{width: "150pt", margin: "0 30pt"}}>
-                    <div className="p-text-center p-col-12">
-                        <b>Pris:</b> {booking.price}
-                    </div>
-                    <div className="p-text-center p-col-12">
-                        <b>Betald:</b> {booking.paid ? "Ja" : "Nej"}
-                    </div>
-                </div>
+            <div className="p-shadow-5 p-m-3 p-col-12" style={{width: "60%"}}>
+                {reservations}
             </div>
-        </div>
-        <div className="p-shadow-5 p-m-3 p-col-12" style={{width: "60%"}}>
-            {reservations}
-        </div>
-        <div className="p-m-3 p-grid" style={{width: "40%"}}>
-            <Button label="Redigera" icon="pi pi-pencil" className="p-button-info p-col p-shadow-5" style={{margin: "0 30pt 0 0"}}/>
-            <Button label="Ta bort" icon="pi pi-minus" className="p-button-danger p-col p-shadow-5" onClick={() => confirmDeleteBooking()}/>
-        </div>
-        <Dialog visible={deleteBookingDialog} style={{ width: '450px' }} header="Bekräfta borttagning" modal footer={deleteBookingDialogFooter} onHide={hideDeleteBookingDialog}>
-            <div className="confirmation-content">
-                <i className="pi pi-exclamation-triangle p-mr-3" style={{ fontSize: '2rem'}} />
-                {booking && <span>Är du säker på att du vill ta bort bokningen?</span>}
+            <div className="p-m-3 p-grid" style={{width: "40%"}}>
+                <Button label="Spara" icon="pi pi-check" className="p-button-success p-col p-shadow-5"
+                        style={{margin: "0 30pt 0 0"}} onClick={() => confirmDeleteBooking()}/>
+                <Button label="Redigera" icon="pi pi-pencil" className="p-button-info p-col p-shadow-5"
+                        style={{margin: "0 30pt 0 0"}} onClick={() => setEditable(!editable)}/>
+                <Button label="Ta bort" icon="pi pi-minus" className="p-button-danger p-col p-shadow-5"
+                        onClick={() => confirmDeleteBooking()}/>
+
             </div>
-        </Dialog>
-    </div>
+            <Dialog visible={deleteBookingDialog} style={{width: '450px'}} header="Bekräfta borttagning" modal
+                    footer={deleteBookingDialogFooter} onHide={hideDeleteBookingDialog}>
+                <div className="confirmation-content">
+                    <i className="pi pi-exclamation-triangle p-mr-3" style={{fontSize: '2rem'}}/>
+                    {booking && <span>Är du säker på att du vill ta bort bokningen?</span>}
+                </div>
+            </Dialog>
+        </div>
 
     );
 }
